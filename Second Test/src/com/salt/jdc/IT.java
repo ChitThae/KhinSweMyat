afd2ab510a4809f1eb699919802f82b7aae427eb
Administrator <Administrator@MR-SONT69GN1IE9>
2017-11-24 00:05:56 -0800

    It Test

--- Second Test/src/com/salt/jdc/IT.java
+++ Second Test/src/com/salt/jdc/IT.java
@@ -1,5 +1,19 @@
 package com.salt.jdc;
 
 public class IT {
+	private String major;
+	private int rollno;
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
 
 }
